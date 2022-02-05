cfc41beaacd1b5f267ec8dd22a60911debe9344b
nike z <nikegreen@yandex.ru>
2022-02-05 19:58:50 +0500

    5.1. Именованные аргументы [#295518]

---
+++ src/main/java/ru/job4j/io/ArgsName.java
@@ -0,0 +1,48 @@
+package ru.job4j.io;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+public class ArgsName {
+
+    private final Map<String, String> values = new HashMap<>();
+
+    public String get(String key) throws IllegalArgumentException {
+        return Optional.ofNullable(values.get(key)).orElseThrow(() -> new IllegalArgumentException("ключ не существует"));
+    }
+
+    private void parse(String[] args) throws IllegalArgumentException {
+        for (String arg : args) {
+            if (!arg.startsWith("-")) {
+                throw new IllegalArgumentException("ключ должен начинаться с '-'");
+            }
+            int index = arg.indexOf("=");
+            if (index == -1) {
+                throw new IllegalArgumentException("Пропущен знак '='");
+            }
+            if (index == 1) {
+                throw new IllegalArgumentException("Пропущен ключ");
+            }
+            if (index == arg.length() - 1) {
+                throw new IllegalArgumentException("Пропущено значение");
+            }
+            String key = arg.substring(1, index);
+            String val = arg.substring(index + 1);
+            values.put(key, val);
+        }
+    }
+
+    public static ArgsName of(String[] args) {
+        ArgsName names = new ArgsName();
+        names.parse(args);
+        return names;
+    }
+
+    public static void main(String[] args) {
+        ArgsName jvm = ArgsName.of(new String[] {"-Xmx=512", "-encoding=UTF-8"});
+        System.out.println(jvm.get("Xmx"));
+
+        ArgsName zip = ArgsName.of(new String[] {"-out=project.zip", "-encoding=UTF-8"});
+        System.out.println(zip.get("out"));
+    }
+}
