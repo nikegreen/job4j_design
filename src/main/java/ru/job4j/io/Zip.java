464ad3e3d8f4311c3ebfe12e2258a78a893d63b7
nike z <nikegreen@yandex.ru>
2022-02-06 22:25:03 +0500

    5.2. Архивировать проект [#861]

---
+++ src/main/java/ru/job4j/io/Zip.java
@@ -0,0 +1,61 @@
+package ru.job4j.io;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+public class Zip {
+
+    public static void packFiles(List<Path> sources, Path target) {
+        try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target.toFile())))) {
+            for (Path sourcePath : sources) {
+                zip.putNextEntry(new ZipEntry(sourcePath.toString()));
+                if (!Files.isDirectory(sourcePath)) {
+                    try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(sourcePath.toFile()))) {
+                        zip.write(out.readAllBytes());
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void packSingleFile(Path source, Path target) {
+        try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target.toFile())))) {
+            zip.putNextEntry(new ZipEntry(source.toString()));
+            try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(source.toFile()))) {
+                zip.write(out.readAllBytes());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        ArgsName argsName = ArgsName.of(args);
+        String source = argsName.get("d");
+        Path sourcePath = Paths.get(source);
+        if (!Files.isDirectory(sourcePath)) {
+            throw new  IllegalArgumentException("Не директория");
+        }
+        String exclude = argsName.get("e");
+        String dest = argsName.get("o");
+        Path destPath = Paths.get(dest);
+
+        SearchFiles searchFiles = new SearchFiles(f -> !Files.isDirectory(f) && !f.endsWith(exclude));
+        Files.walkFileTree(sourcePath, searchFiles);
+        packFiles(searchFiles.getPaths(), destPath);
+        searchFiles.getPaths().forEach(System.out::println);
+        /**
+        packSingleFile(
+                Paths.get("./pom.xml"),
+                Paths.get("./pom.zip")
+        );
+         */
+    }
+}
