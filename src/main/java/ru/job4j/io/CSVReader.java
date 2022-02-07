c647fb2c1e168a5ffd1804b62acf2d2fdc030b32
nike z <nikegreen@yandex.ru>
2022-02-07 22:14:26 +0500

    7. Scanner [#504791 #261172]

--- src/main/java/ru/job4j/io/CSVReader.java
+++ src/main/java/ru/job4j/io/CSVReader.java
@@ -86,6 +86,8 @@ public class CSVReader {
                 }
                 pw.println();
             }
+            pw.flush();
+            pw.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
