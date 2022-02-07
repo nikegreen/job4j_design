7d2a058e5b9221cee06577813622139a94932a8b
nike z <nikegreen@yandex.ru>
2022-02-07 21:33:23 +0500

    7. Scanner [#504791 #261172]

--- src/main/java/ru/job4j/io/ArgsName.java
+++ src/main/java/ru/job4j/io/ArgsName.java
@@ -8,25 +8,25 @@ public class ArgsName {
     private final Map<String, String> values = new HashMap<>();
 
     public String get(String key) throws IllegalArgumentException {
-        return Optional.ofNullable(values.get(key)).orElseThrow(() -> new IllegalArgumentException("ключ не существует"));
+        return Optional.ofNullable(values.get(key)).orElseThrow(() -> new IllegalArgumentException("ключ '" + key + "' не существует"));
     }
 
     private void parse(String[] args) throws IllegalArgumentException {
         for (String arg : args) {
             if (!arg.startsWith("-")) {
-                throw new IllegalArgumentException("ключ должен начинаться с '-'");
+                throw new IllegalArgumentException("ключ должен начинаться с '-' (" + arg + ")");
             }
             int index = arg.indexOf("=");
             if (index == -1) {
-                throw new IllegalArgumentException("Пропущен знак '='");
+                throw new IllegalArgumentException("Пропущен знак '=' (" + arg + ")");
             }
             if (index == 1) {
-                throw new IllegalArgumentException("Пропущен ключ");
-            }
-            if (index == arg.length() - 1) {
-                throw new IllegalArgumentException("Пропущено значение");
+                throw new IllegalArgumentException("Пропущен ключ (" + arg + ")");
             }
             String key = arg.substring(1, index);
+            if (index == arg.length() - 1) {
+                throw new IllegalArgumentException("Пропущено значение ключа '" + key + "' (" + arg + ")");
+            }
             String val = arg.substring(index + 1);
             values.put(key, val);
         }
--- src/main/java/ru/job4j/io/CSVReader.java
+++ src/main/java/ru/job4j/io/CSVReader.java
@@ -2,9 +2,33 @@ package ru.job4j.io;
 
 import java.io.*;
 import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.*;
 
 public class CSVReader {
+    public static void validate(ArgsName argsName) throws IllegalArgumentException {
+        String path = argsName.get("path");
+        if (!Files.exists(Paths.get(path))) {
+            throw new IllegalArgumentException("исходного файла не существует (" + path + ")");
+        }
+        if (Files.isDirectory(Paths.get(path))) {
+            throw new IllegalArgumentException("нужно имя исходного файла, а не каталог (" + path + ")");
+        }
+        String delimiter = argsName.get("delimiter");
+        String out = argsName.get("out");
+        String filter = argsName.get("filter");
+        List<String> filters = List.of(filter.split(","));
+        if (filter.length() == 0 || filters.size() == 0) {
+            throw new IllegalArgumentException("не указаны столбцы");
+        }
+        for (String s : filters) {
+            if (s.length() == 0) {
+                throw new IllegalArgumentException("пропущено название столбца");
+            }
+        }
+    }
 /**
  * CSVReader.handle - копирует столбцы из файла типа .csv в другой .csv файл
  * ожидаются аргументы:
@@ -13,16 +37,19 @@ public class CSVReader {
  * out - путь и имя .csv файла с результатом
  * filter - имена столбцов, которые нужно скопировать (перечислены через символ ',')
  */
-    public static void handle(ArgsName argsName) throws Exception {
-        String path = argsName.get("path");
-        String delimiter = argsName.get("delimiter");
-        String out = argsName.get("out");
-        String filter = argsName.get("filter");
-        List<String> filters = List.of(filter.split(","));
-        Set<Integer> setFilters = new HashSet<>();
-        try (BufferedReader br = new BufferedReader(new FileReader(path, Charset.forName("UTF-8")));
-             PrintWriter pw = new PrintWriter(
-                     new FileWriter(out, Charset.forName("UTF-8"), true))) {
+    public static void handle(ArgsName argsName) {
+        try {
+            validate(argsName);
+            String path = argsName.get("path");
+            String delimiter = argsName.get("delimiter");
+            String out = argsName.get("out");
+            String filter = argsName.get("filter");
+            List<String> filters = List.of(filter.split(","));
+            Set<Integer> setFilters = new HashSet<>();
+            BufferedReader br = new BufferedReader(new FileReader(path, Charset.forName("UTF-8")));
+            PrintWriter pw = "stdout".equals(out)
+                    ? new PrintWriter(System.out)
+                    : new PrintWriter(new FileWriter(out, Charset.forName("UTF-8"), true));
             Scanner scan1 = new Scanner(br);
             if (scan1.hasNextLine()) {
                 Scanner scan2 = new Scanner(scan1.nextLine()).useDelimiter(delimiter);
@@ -60,6 +87,15 @@ public class CSVReader {
                 pw.println();
             }
         } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        ArgsName argsName = ArgsName.of(args);
+        try {
+            handle(argsName);
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
