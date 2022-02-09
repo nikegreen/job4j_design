42f0c9690a65c0566c41b6a524b76bbd1d573841
nike z <nikegreen@yandex.ru>
2022-02-10 00:26:50 +0500

    2. Поиск файлов по критерию [#783]

---
+++ src/main/java/ru/job4j/io/find/Find.java
@@ -0,0 +1,63 @@
+package ru.job4j.io.find;
+
+import ru.job4j.io.ArgsName;
+import ru.job4j.io.Search;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.*;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.regex.Pattern;
+
+import static java.util.regex.Pattern.matches;
+
+public class Find {
+    private static Path path;
+    private static String mask;
+    private static int type;
+    private static String out;
+    private static Predicate<Path> condition;
+
+    private static void validate(String[] args) {
+        ArgsName argsName = ArgsName.of(args);
+        String pathStr = argsName.get("d");
+        path = Paths.get(pathStr);
+        if (!Files.isDirectory(path)) {
+            throw new IllegalArgumentException("Не каталог (" + pathStr + ")");
+        }
+        mask = argsName.get("n");
+        String typeStr = argsName.get("t");
+        PathMatcher matcher;
+        switch (typeStr) {
+            case "file":
+                condition = f -> mask.equals(f.getFileName().toString());
+                break;
+            case "mask":
+                matcher = FileSystems.getDefault().getPathMatcher("glob:" + mask);
+                condition = f -> matcher.matches(f);
+                break;
+            case "regex":
+                condition = f -> matches(mask, f.toString());
+                break;
+            default:
+                throw new IllegalArgumentException("недопустимый тип ключа -t (" + typeStr + ")");
+        }
+        out = argsName.get("o");
+    }
+
+    private static void find(String[] args) throws IOException {
+        validate(args);
+        List<Path> list = Search.search(path, condition);
+        try (PrintWriter pw = new PrintWriter(new FileWriter(out))) {
+            list.forEach(pw::println);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        find(args);
+    }
+}
