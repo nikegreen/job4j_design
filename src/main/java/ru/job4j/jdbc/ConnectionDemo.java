8860a7984c1063ceacf62754bf413136ba510095
nike z <nikegreen@yandex.ru>
2022-02-27 15:15:24 +0500

    0. JDBC [#6863]

---
+++ src/main/java/ru/job4j/jdbc/ConnectionDemo.java
@@ -0,0 +1,32 @@
+package ru.job4j.jdbc;
+
+import ru.job4j.io.Config;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionDemo {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        final String path = "app.properties";
+        Class.forName("org.postgresql.Driver");
+        String url;
+        String login;
+        String password;
+        try {
+            Config config = new Config(path);
+            config.load();
+            url = config.value("url");
+            login = config.value("login");
+            password = config.value("password");
+            try (Connection connection = DriverManager.getConnection(url, login, password)) {
+                DatabaseMetaData metaData = connection.getMetaData();
+                System.out.println(metaData.getUserName());
+                System.out.println(metaData.getURL());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
